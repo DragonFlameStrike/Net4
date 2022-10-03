aad2aadef30909326e9de6fad79c32f69a1e7f18
DragonFlameStrike <d.pankov@g.nsu.ru>
2022-10-03 21:18:11 +0700

    init commit

--- main.go
+++ main.go
@@ -1,1 +1,61 @@
 package main
+
+import (
+	"fmt"
+	e "github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"os"
+)
+
+const (
+	CLOSE = iota
+	CREATE
+	CONNECT
+)
+
+func update(screen *e.Image) error {
+	img, _, _ := ebitenutil.NewImageFromFile("./images/BackGround.png", e.FilterDefault)
+	screen.DrawImage(img, nil)
+	return nil
+}
+
+func main() {
+	greetingsMainMenu()
+	c := getConsoleStartChoose()
+	switch c {
+	case CLOSE:
+		os.Exit(1)
+	case CREATE:
+		createGame()
+	case CONNECT:
+		findGames()
+	}
+}
+
+func createGame() {
+	e.Run(update, 800, 600, 1, "Snakes")
+}
+
+func findGames() {
+
+}
+
+func getConsoleStartChoose() int {
+	for {
+		c := -1
+		fmt.Fscan(os.Stdin, &c)
+		if c > 3 || c < 0 {
+			fmt.Println("Wrong input")
+			continue
+		}
+		return c
+	}
+}
+
+func greetingsMainMenu() {
+	fmt.Println("Hello User!")
+	fmt.Println("Choose what you want:")
+	fmt.Println("1. Create a new game")
+	fmt.Println("2. Connect to the game")
+	fmt.Println("0. Close the game")
+}

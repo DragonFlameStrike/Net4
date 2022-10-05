14370f9b2280bc8cb85a5a72d3fb11fb49a43050
DragonFlameStrike <d.pankov@g.nsu.ru>
2022-10-05 11:20:03 +0700

    add multicast sender

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	e "github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"log"
+	"net"
 	"os"
+	"time"
 )
 
 const (
 	CLOSE = iota
 	CREATE
 	CONNECT
+	srvAddr         = "239.192.0.4:9192"
+	maxDatagramSize = 8192
 )
 
 func update(screen *e.Image) error {
@@ -22,6 +28,8 @@ func update(screen *e.Image) error {
 func main() {
 	greetingsMainMenu()
 	c := getConsoleStartChoose()
+	go serveMulticastUDP(srvAddr, msgHandler)
+	go ping(srvAddr, "hello")
 	switch c {
 	case CLOSE:
 		os.Exit(1)
@@ -29,6 +37,46 @@ func main() {
 		createGame()
 	case CONNECT:
 		findGames()
+	}
+}
+
+func ping(a string, massage string) {
+	addr, err := net.ResolveUDPAddr("udp", a)
+	if err != nil {
+		log.Fatal(err)
+	}
+	c, err := net.DialUDP("udp", nil, addr)
+	for i := 1; i < 1000; i++ {
+		if err != nil {
+			fmt.Println("Error listening:", err)
+			os.Exit(1)
+		}
+		c.Write([]byte(massage))
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func msgHandler(src *net.UDPAddr, n int, b []byte) {
+	log.Println(hex.Dump(b[:n]))
+}
+
+func serveMulticastUDP(a string, h func(*net.UDPAddr, int, []byte)) {
+	addr, err := net.ResolveUDPAddr("udp", a)
+	if err != nil {
+		log.Fatal(err)
+	}
+	l, err := net.ListenMulticastUDP("udp", nil, addr)
+	err = l.SetReadBuffer(maxDatagramSize)
+	if err != nil {
+		return
+	}
+	for {
+		b := make([]byte, maxDatagramSize)
+		n, src, err := l.ReadFromUDP(b)
+		if err != nil {
+			log.Fatal("ReadFromUDP failed:", err)
+		}
+		h(src, n, b)
 	}
 }
 
